cf07d40fd90e1dd777237970230957b36c0de49c
mreijn <mreijn@schubergphilis.com>
2015-04-23 13:45:20 +0200

    dump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'avanstraten@schubergphilis.com'
 license          'All rights reserved'
 description      'Installs/Configures SafeNet Client'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 supports         'windows'
 
