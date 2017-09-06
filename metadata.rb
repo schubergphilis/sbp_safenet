8cf0f02074283bb0182869ea5f4034fcbc8db010
Suleyman Kutlu <skutlu@schubergphilis.com>
2017-09-06 13:37:49 +0200

    version bump up

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'avanstraten@schubergphilis.com'
 license          'All rights reserved'
 description      'Installs/Configures SafeNet Client'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.2'
+version          '0.1.3'
 
 chef_version '>= 11' if respond_to?(:chef_version)
 
